8bdf841364235f9763b91f0965c89422a507b36d
hmkhan <hmkhan@gmail.com>
2024-05-08 11:25:39 -0700

    first commit

---
+++ src/main/java/org/base/App.java
@@ -0,0 +1,49 @@
+package org.base;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.base.handlers.CartServlet;
+import org.base.handlers.DirectoryServlet;
+import org.base.security.BasicAuth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static Server jetty;
+    public static void main(String[] args) throws Exception {
+
+        jetty = new Server(8080);
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSendServerVersion(false);
+        HttpConnectionFactory httpFactory = new HttpConnectionFactory(httpConfig);
+
+        Injector injector = Guice.createInjector();
+
+        ServletContextHandler servletContextHandler =
+                new ServletContextHandler(jetty, "/", true, true);
+
+        //TODO: this should be removed from here; Setup auth; Basic cm9vdDpwYXNzd29yZA==
+        servletContextHandler.setSecurityHandler(
+                BasicAuth.basicAuth("root", "password", "base!"));
+
+        DirectoryServlet directoryServlet = injector.getInstance(DirectoryServlet.class);
+        servletContextHandler.addServlet(new ServletHolder(directoryServlet), "/directory/*");
+
+        logger.info("Starting the server!!");
+        jetty.start();
+        jetty.join();
+
+    }
+
+}
---
+++ src/main/java/org/base/security/BasicAuth.java
@@ -0,0 +1,41 @@
+package org.base.security;
+
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.security.UserStore;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Credential;
+import org.eclipse.jetty.util.security.Password;
+
+public class BasicAuth {
+
+    public static final SecurityHandler basicAuth(String username, String password, String realm) {
+
+        UserStore userStore = new UserStore();
+        userStore.addUser(username, new Password(password), new String[] { "users"});
+
+        HashLoginService hashLoginService = new HashLoginService(realm);
+        hashLoginService.setUserStore(userStore);
+
+        Constraint constraint = new Constraint();
+        constraint.setName(Constraint.__BASIC_AUTH);
+        constraint.setRoles(new String[]{"users"});
+        constraint.setName(username);
+        constraint.setAuthenticate(true);
+
+        ConstraintMapping constraintMapping = new ConstraintMapping();
+        constraintMapping.setConstraint(constraint);
+        constraintMapping.setPathSpec("/*");
+
+        ConstraintSecurityHandler constraintSecurityHandler = new ConstraintSecurityHandler();
+        constraintSecurityHandler.setAuthenticator(new BasicAuthenticator());
+        constraintSecurityHandler.setLoginService(hashLoginService);
+        constraintSecurityHandler.addConstraintMapping(constraintMapping);
+        constraintSecurityHandler.setRealmName(realm);
+
+        return constraintSecurityHandler;
+    }
+}
